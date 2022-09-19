df59b7179e309e59f9727e9ab11ad6e702eaeff6
Stas Grigoryev <stasilius2009@gmail.com>
2022-09-20 01:53:17 +0300

    aop - annotation pointcut

---
+++ src/main/java/com/home/chapter05/annotationPointcut/Guitar.java
@@ -0,0 +1,7 @@
+package com.home.chapter05.annotationPointcut;
+
+public class Guitar {
+    public String play() {
+        return "Cm Eb Fm Ab Bb annotated";
+    }
+}
---
+++ src/main/java/com/home/chapter05/annotationPointcut/Singer.java
@@ -0,0 +1,7 @@
+package com.home.chapter05.annotationPointcut;
+
+public interface Singer {
+    void sing();
+    void sing(Guitar guitar);
+    void rest();
+}
